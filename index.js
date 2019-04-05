4501e2bb757d0ee6a9a82a0ba65d61577f8c7c6f
Mohanapriya <priya05mca@gmail.com>
2019-04-05 17:13:56 +0100

    final commit

--- index.js
+++ index.js
@@ -53,13 +53,57 @@ const server = http.createServer((req, res) => {
   } */ //static file path
 
   //dynamic filepath
-  let filepath = path.join(
+  let filePath = path.join(
     __dirname,
     "public",
     req.url === "/" ? "index.html" : req.url
   );
-  console.log(filepath);
-  res.end();
+  //   console.log(filePath);
+  //   res.end();
+  let extName = path.extname(filePath);
+  let contentType = "text/html";
+
+  //check extension and set content type
+  switch (extName) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+  //readfile
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        //page not found
+        fs.readFile(
+          path.join(__dirname, "public", "e404.html"),
+          (err, content) => {
+            res.writeHead(200, { "content-type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        //some server error
+        res.writeHead(500);
+        res.end(`server Error: ${err.code}`);
+      }
+    } else {
+      //success
+      res.writeHead(200, { "content-type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
